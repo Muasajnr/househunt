d4983907ea2169dcd082d95d2c12e337575bf1bc
Muasajnr <116666398+Muasajnr@users.noreply.github.com>
2024-01-28 23:40:29 +0300

    #commit 2

--- househunt/lib/main.dart
+++ househunt/lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
           //
-          // Column has various properties to control how it sizes itself and
+          // Column has various properties to control how it  sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
